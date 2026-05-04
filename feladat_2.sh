0039c1bea2cf763451231d5f1ad4ab52dc491e16
6gDav <c6gdavprog0402@gmail.com>
2026-05-04 16:03:51 +0200

    feat: 2th task is done

---
+++ feladat_2.sh
@@ -0,0 +1,18 @@
+# 2. Sorok számolása több fájlban
+# Kérdés:
+# Kérjen be több fájlnevet, majd írja ki külön-külön, hogy hány sor van bennük.
+
+#!/bin/bash
+
+read -p "Hany filet karsz beolvasni " ncounter
+
+for (( i=1; i<=$ncounter; i++ ))
+do  
+    read -p "A file neve: " filenev
+
+    if [ -e "$filenev" ]
+    then
+        sorokszama=$(cat "$filenev" | wc -l)
+        echo "Sorok szama a $sorokszama"
+    fi
+done
