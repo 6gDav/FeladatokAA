0b60728428c5904e0b9b7c2f6f7a58c0ab81e417
6gDav <c6gdavprog0402@gmail.com>
2026-05-04 15:51:29 +0200

    feat: 1th task

---
+++ feladat_1.sh
@@ -0,0 +1,7 @@
+# 1. Fájlok listázása dátum szerint
+# Kérdés:
+# Írj scriptet, amely egy megadott mappában kilistázza a fájlokat módosítási dátum szerint csökkenő sorrendben.
+
+#!/bin/bash
+
+ls -ltr
