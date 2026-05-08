e2de02b9ad40de4c70869cd3e02026545ee618a6
6gDav <c6gdavprog0402@gmail.com>
2026-05-08 16:05:28 +0200

    feat: 7th task is done

---
+++ feladat_7.sh
@@ -0,0 +1,16 @@
+# 7. Egyszerű naplózás (logging)
+# Kérdés:
+# Írj scriptet, amely minden futtatáskor hozzáfűzi az aktuális dátumot és egy üzenetet egy log.txt fájlhoz.
+
+#!/bin/bash
+
+file_name="log.txt"
+datum=$(date)
+
+if [ -e $file_name ]
+then
+    echo "$datum" >> $file_name  
+else
+    touch $file_name
+    echo "$datum" > $file_name 
+fi
